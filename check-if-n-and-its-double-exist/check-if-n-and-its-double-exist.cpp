89ee838aeda36756a955449c5ea473c4e0e7fe65
Shyamansh <89519907+Shyamansh@users.noreply.github.com>
2022-08-26 18:37:08 +0530

    Time: 13 ms, Memory: 9.9 MB - LeetHub

---
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        for(int i=0 ; i<arr.size() ; ++i)
+        {
+            for(int j=0 ; j<arr.size() ; ++j)
+            {
+                if(arr[i] == 2*arr[j] && i != j)
+                {
+                    cout<<"true for i"<<i<<" j"<<j<<endl;
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
